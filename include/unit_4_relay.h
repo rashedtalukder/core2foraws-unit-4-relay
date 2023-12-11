175b47bff8bb2933ac20c157c192637308cc18c2
Rashed Talukder <9218468+rashedtalukder@users.noreply.github.com>
2023-12-11 13:08:17 -0800

    Add methods for getting channel states. Add macros for device modes

--- include/unit_4_relay.h
+++ include/unit_4_relay.h
@@ -1,12 +1,12 @@
 /*!
  * @brief Library for the 4 relay unit by M5Stack on the Core2 for AWS
- * @copyright Copyright (c) 2022 by Rashed Talukder[https://rashedtalukder.com]
+ * @copyright Copyright (c) 2023 by Rashed Talukder[https://rashedtalukder.com]
  * 
  * TODO: Return a fail for attempting to change LED in sync mode
  *
  * @Links [4-Relay](https://docs.m5stack.com/en/unit/4relay)
- * @version  V0.0.1
- * @date  2022-11-09
+ * @version  V0.0.2
+ * @date  2023-12-11
  */
 #ifndef _UNIT_4_RELAY_H_
 #define _UNIT_4_RELAY_H_
@@ -17,9 +17,29 @@ extern "C" {
 
 #include <esp_err.h>
 
-#define UNIT_4_RELAY_ADDR      0x26
-#define UNIT_4_RELAY_REG_MODE  0x10
-#define UNIT_4_RELAY_REG_RELAY 0x11
+#define UNIT_4_RELAY_ADDR           0x26
+#define UNIT_4_RELAY_REG_MODE       0x10
+#define UNIT_4_RELAY_REG_RELAY      0x11
+
+/**
+ * @brief Enable the LEDs and relays to be controlled individually.
+ * 
+ * In asynchronous mode, the LEDs are controlled independently of the relay states. Meaning 
+ * the relay can be closed (on) and the LED will not change without setting the state of 
+ * that specific LED channel to turn on using @ref unit_4_relay_led_set.
+ * 
+ */
+#define UNIT_4_RELAY_MODE_ASYNC     0
+
+/**
+ * @brief Enable the LEDs and relays to be controlled individually.
+ * 
+ * In asynchronous mode, the LEDs are controlled independently of the relay states. Meaning 
+ * the relay can be closed (on) and the LED will not change without setting the state of 
+ * that specific LED channel to turn on using @ref unit_4_relay_led_set.
+ * 
+ */
+#define UNIT_4_RELAY_MODE_SYNC      1
 
 /** 
  * @brief Initialize the mode of the unit and turn off all relays.
@@ -39,8 +59,18 @@ extern "C" {
 esp_err_t unit_4_relay_init( bool mode );
 
 /** 
+ * @brief Get the on/off state of the specified relay.
+ * @param channel Channel number of relay (0-3).
+ * @param state OFF = 0, ON = 1.
+ * @return [esp_err_t](https://docs.espressif.com/projects/esp-idf/en/release-v4.3/esp32/api-reference/system/esp_err.html#macros).
+ *  - ESP_OK                : Success
+ *  - ESP_ERR_INVALID_ARG	: Driver parameter error
+ */
+esp_err_t unit_4_relay_relay_get( uint8_t channel, bool *state );
+
+/** 
  * @brief Set the on/off state of the specified relay.
- * @param channel Relay channel number of relay (0~3).
+ * @param channel Channel number of relay (0-3).
  * @param state OFF = 0, ON = 1.
  * @return [esp_err_t](https://docs.espressif.com/projects/esp-idf/en/release-v4.3/esp32/api-reference/system/esp_err.html#macros).
  *  - ESP_OK                : Success
@@ -49,8 +79,18 @@ esp_err_t unit_4_relay_init( bool mode );
 esp_err_t unit_4_relay_relay_set( uint8_t channel, bool state );
 
 /** 
+ * @brief Gets the on/off state of the specified LED.
+ * @param channel Channel of LED (0-3).
+ * @param state OFF = 0, ON = 1.
+ * @return [esp_err_t](https://docs.espressif.com/projects/esp-idf/en/release-v4.3/esp32/api-reference/system/esp_err.html#macros).
+ *  - ESP_OK                : Success
+ *  - ESP_ERR_INVALID_ARG	: Driver parameter error
+ */
+esp_err_t unit_4_relay_led_get( uint8_t channel, bool *state );
+
+/** 
  * @brief Set the on/off state of the specified LED.
- * @param channel Relay channel of LED (0~3).
+ * @param channel Channel of LED (0-3).
  * @param state OFF = 0, ON = 1.
  * @return [esp_err_t](https://docs.espressif.com/projects/esp-idf/en/release-v4.3/esp32/api-reference/system/esp_err.html#macros).
  *  - ESP_OK                : Success
@@ -69,12 +109,12 @@ esp_err_t unit_4_relay_relay_all( bool state );
 
 /** 
  * @brief Set the mode of the unit.
- * @param mode Async = 0, Sync = 1.
+ * @param async_mode Async = 0, Sync = 1.
  * @return [esp_err_t](https://docs.espressif.com/projects/esp-idf/en/release-v4.3/esp32/api-reference/system/esp_err.html#macros).
  *  - ESP_OK                : Success
  *  - ESP_ERR_INVALID_ARG	: Driver parameter error
  */
-esp_err_t unit_4_relay_mode_set( bool *mode );
+esp_err_t unit_4_relay_mode_set( bool *async_mode );
 
 #ifdef __cplusplus
 }
--- unit_4_relay.c
+++ unit_4_relay.c
@@ -6,11 +6,35 @@
 
 static const char *_TAG = "UNIT_4_RELAY";
 
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c\n"
+#define BYTE_TO_BINARY(byte)  \
+  ((byte) & 0x80 ? '1' : '0'), \
+  ((byte) & 0x40 ? '1' : '0'), \
+  ((byte) & 0x20 ? '1' : '0'), \
+  ((byte) & 0x10 ? '1' : '0'), \
+  ((byte) & 0x08 ? '1' : '0'), \
+  ((byte) & 0x04 ? '1' : '0'), \
+  ((byte) & 0x02 ? '1' : '0'), \
+  ((byte) & 0x01 ? '1' : '0')
+
 esp_err_t unit_4_relay_init( bool mode )
 {
     ESP_LOGD( _TAG, "Initializing" );
     esp_err_t err = unit_4_relay_mode_set( &mode );
     return err |= unit_4_relay_relay_all( 0 );
+}
+
+esp_err_t unit_4_relay_relay_get( uint8_t channel, bool *state )
+{
+    uint8_t reg_val;
+    ESP_LOGD( _TAG, "Getting channel %d state .", channel );
+    esp_err_t err = core2foraws_expports_i2c_read( UNIT_4_RELAY_ADDR, UNIT_4_RELAY_REG_RELAY, &reg_val, 1 );
+
+    ESP_LOGD( _TAG, "Received state from register (first 4 LED's last 4, relays): "BYTE_TO_BINARY_PATTERN, 
+        BYTE_TO_BINARY( reg_val ) );
+    
+    *state = reg_val & ( 1 << channel );
+    return err;
 }
 
 esp_err_t unit_4_relay_relay_set( uint8_t channel, bool state )
@@ -20,7 +44,7 @@ esp_err_t unit_4_relay_relay_set( uint8_t channel, bool state )
     uint8_t current_state;
     
     core2foraws_expports_i2c_read( UNIT_4_RELAY_ADDR, UNIT_4_RELAY_REG_RELAY, &current_state, 1 );
-        
+
     if ( state == 0 )
     {
         current_state &= ~( 0x01 << channel );
@@ -30,6 +54,19 @@ esp_err_t unit_4_relay_relay_set( uint8_t channel, bool state )
         current_state |= ( 0x01 << channel );
     }
     return core2foraws_expports_i2c_write( UNIT_4_RELAY_ADDR, UNIT_4_RELAY_REG_RELAY, &current_state, 1 );
+}
+
+esp_err_t unit_4_relay_led_get( uint8_t channel, bool *state )
+{
+    uint8_t reg_val;
+    ESP_LOGD( _TAG, "Getting channel %d state .", channel );
+    esp_err_t err = core2foraws_expports_i2c_read( UNIT_4_RELAY_ADDR, UNIT_4_RELAY_REG_RELAY, &reg_val, 1 );
+
+    ESP_LOGD( _TAG, "Received state from register (first 4 LED's last 4, relays): "BYTE_TO_BINARY_PATTERN, 
+        BYTE_TO_BINARY( reg_val ) );
+    
+    *state = ( reg_val >> 0x04 ) & ( 1 << channel );
+    return err;
 }
 
 esp_err_t unit_4_relay_led_set( uint8_t channel, bool state )
@@ -59,8 +96,8 @@ esp_err_t unit_4_relay_relay_all( bool state )
     return core2foraws_expports_i2c_write( UNIT_4_RELAY_ADDR, UNIT_4_RELAY_REG_RELAY, &new_state, 1 ); 
 }
 
-esp_err_t unit_4_relay_mode_set( bool *mode )
+esp_err_t unit_4_relay_mode_set( bool *async_mode )
 {
-    ESP_LOGD( _TAG, "Setting mode to %s mode.", mode ? "synchronous" : "asynchronous" );
-    return core2foraws_expports_i2c_write( UNIT_4_RELAY_ADDR, UNIT_4_RELAY_REG_MODE, (const uint8_t *)mode, 1 ); 
+    ESP_LOGD( _TAG, "Setting mode to %s mode.", async_mode ? "synchronous" : "asynchronous" );
+    return core2foraws_expports_i2c_write( UNIT_4_RELAY_ADDR, UNIT_4_RELAY_REG_MODE, (const uint8_t *)async_mode, 1 ); 
 }
